799bd4f20511682752001089ce4eb99182a6cc46
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-26 18:07:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  obj[driver] = value;
+  driver[key] = value;
   return 'Sam', '12 Broadway';
 }
