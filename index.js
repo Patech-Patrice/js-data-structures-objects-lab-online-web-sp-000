2040a10aa51b5ba165346b0ac94aa8942d4de1de
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-26 18:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const driver = {
+"name": "Sam",
+"address": "11 Broadway",
+};
+
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  var Copy = Object.assign({}, driver, {[key]: value});
+  return Copy
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  obj[name,address] = value;
+  return 'Sam', '12 Broadway';
+}
